f0c22a25bf5fa136182ff4330cc571062826de12
Alan-Muk <alanmukab@gmail.com>
2026-06-15 07:03:38 +0300

    Backend Update

--- Frontend/src/services/api.tsx
+++
@@ -1,9 +0,0 @@
-import { GraphData } from "../types/graph.types";
-
-const BASE_URL = "http://localhost:3000";
-
-export async function fetchWordGraph(word: string): Promise<GraphData> {
-  const res = await fetch(`${BASE_URL}/word/${word}`);
-  if (!res.ok) throw new Error("API error");
-  return res.json();
-}
